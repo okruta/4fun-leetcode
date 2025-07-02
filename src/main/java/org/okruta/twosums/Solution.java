1bc185850f2f41ac7452b7bf3ca296c3e3d14784
Walter Okruciński <walterokrutny@gmail.com>
2025-07-02 12:32:08 +0200

    Two sums problem

---
+++ src/main/java/org/okruta/twosums/Solution.java
@@ -0,0 +1,27 @@
+package org.okruta.twosums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> neededMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
+            int needed = target - a;
+            Integer indexOfNeeded = neededMap.get(a);
+            if (indexOfNeeded != null) {
+                return new int[]{indexOfNeeded, i};
+            }
+            neededMap.put(needed, i);
+        }
+        throw new IllegalArgumentException("No result");
+    }
+}
---
+++ src/test/java/org/okruta/twosums/TowSumsTest.java
@@ -0,0 +1,33 @@
+package org.okruta.twosums;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TowSumsTest {
+    private final Solution solution = new Solution();
+
+    private static Stream<Arguments> testArgs() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
+                Arguments.of(new int[]{3, 2, 4}, 6, new int[]{1, 2}),
+                Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1})
+        );
+    }
+
+    @ParameterizedTest(name = "Solution for input = {0} and target = {1} is {2} ")
+    @MethodSource("testArgs")
+    public void shouldReturnTwoSumsSolution(int[] input, int target, int[] expected) {
+        // When
+        int[] result = solution.twoSum(input, target);
+
+        // Then
+        assertThat(result)
+                .isEqualTo(expected);
+    }
+
+}
