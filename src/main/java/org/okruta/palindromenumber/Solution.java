75771d18742cc7ea388b48712a38cb563e6a6f02
Walter Okruciński <walterokrutny@gmail.com>
2025-08-02 12:46:12 +0200

    Palindrome Number

--- src/main/java/org/okruta/palindromenumber/Solution.java
+++ src/main/java/org/okruta/palindromenumber/Solution.java
@@ -6,6 +6,12 @@ package org.okruta.palindromenumber;
 public class Solution {
 
     public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
         String s = Integer.toString(x);
         int length = s.length();
         int invertedIndex = length - 1;
